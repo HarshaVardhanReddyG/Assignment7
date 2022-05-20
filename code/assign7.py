4eec5873ab9c18326f13c0ee90a15ed67face187
HarshaVardhanReddyG <102507828+HarshaVardhanReddyG@users.noreply.github.com>
2022-05-20 10:20:14 +0530

    Update assign7.py

--- code/assign7.py
+++ code/assign7.py
@@ -2,4 +2,4 @@ from scipy.stats import binom
 
 x=binom.pmf(2,7,1/6)
 
-print("Theoretical probability is 0.2344. Practical probability is {0:.3f}".format(x))
+print("Required probability is {0:.3f}".format(x))
